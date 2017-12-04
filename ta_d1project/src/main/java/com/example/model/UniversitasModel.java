5a3525a64ce5d1ca457a7a9fee580bf6ae3a4663
Najwa <najwa.satirah@ui.ac.id>
2017-12-04 19:31:10 +0700

    Add univmodel, update navbar in templates

--- ta_d1project/src/main/java/com/example/model/UniversitasModel.java
+++ ta_d1project/src/main/java/com/example/model/UniversitasModel.java
@@ -1,5 +1,7 @@
 package com.example.model;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +9,9 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ProdiModel
+public class UniversitasModel
 {
-    private int id;
-    private String id_fakultas;
-    private String kode_prodi;
-    private String nama_prodi;
+    private int id_univ;
+    private String nama_univ;
+    private List<FakultasModel> listFakultas;
 }
