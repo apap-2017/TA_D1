c53cafb6cf26e13ab12de613a0e806c20e614c4f
DESKTOP-N7N7QOL\ASUS <gitafazhaf@gmail.com>
2017-12-04 12:15:17 +0700

    update typo

--- ta_d1project/src/main/java/com/example/controller/SIKurikulumController.java
+++ ta_d1project/src/main/java/com/example/controller/SIKurikulumController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class SIKurikulumController {
 	@Autowired
-	//ProductService productDAO;
 
 	@RequestMapping("/")
 	public String index() {
