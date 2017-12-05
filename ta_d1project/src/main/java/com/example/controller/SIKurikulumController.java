91670ba5a7d9aea103758fef1b794edb8d9a65a3
Rifka Nur Fadhilah <rifka.nur@ui.ac.id>
2017-12-05 18:32:15 +0700

    update kurikulum

--- ta_d1project/src/main/java/com/example/controller/SIKurikulumController.java
+++ ta_d1project/src/main/java/com/example/controller/SIKurikulumController.java
@@ -70,7 +70,7 @@ public class SIKurikulumController {
 
 		if (kurikulum != null) {
 			model.addAttribute("kurikulum", kurikulum);
-			return "kurikulum-update";
+			return "kurikulum-update-1";
 		} else {
 			model.addAttribute("id", id);
 			return "kurikulum-not-found";
