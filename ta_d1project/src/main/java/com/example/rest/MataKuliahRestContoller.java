118778903d13490718927c12243cf02b5d2a2c3c
Najwa <najwa.satirah@ui.ac.id>
2017-12-12 22:42:46 +0700

    api getMatkulKurikulum by id

--- ta_d1project/src/main/java/com/example/rest/MataKuliahRestContoller.java
+++ ta_d1project/src/main/java/com/example/rest/MataKuliahRestContoller.java
@@ -1,7 +1,6 @@
 package com.example.rest;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.ApiModel;
-import com.example.model.MataKuliahKurikulumModel;
 import com.example.model.MataKuliahModel;
-import com.example.service.MataKuliahKurikulumService;
 import com.example.service.MataKuliahService;
 
 @RestController
@@ -20,8 +17,6 @@ import com.example.service.MataKuliahService;
 public class MataKuliahRestContoller {
 	@Autowired
 	MataKuliahService mataKuliahService;
-	@Autowired
-	MataKuliahKurikulumService matkulKurikulumService;
 	
 	@RequestMapping("/getMataKuliah/{id}")
 	public ApiModel<Map <String, MataKuliahModel>> getMataKuliah (@PathVariable (value= "id") String id) {
@@ -41,39 +36,6 @@ public class MataKuliahRestContoller {
 			map.put("matkul", matkul);
 				
 			if(matkul != null) {
-				api.setResult(map);
-				api.setMsg("success");
-				api.setStatus(200);
-			} else {
-				tes = false;
-			}
-		}
-		 if (!tes) {
-			api.setMsg("Mata Kuliah tidak ditemukan");
-			api.setStatus(404);
-		}
-		
-		return api;
-	}
-	
-	@RequestMapping("/getMataKuliahByTerm/{term}")
-	public ApiModel<Map <String, List<MataKuliahKurikulumModel>>> getMataKuliahTerm (@PathVariable (value= "term") String term) {
-		int term_matkul = 0;
-		boolean tes = true;
-		ApiModel<Map <String, List<MataKuliahKurikulumModel>>> api = new ApiModel<Map <String, List<MataKuliahKurikulumModel>>>();
-		
-		try {
-			term_matkul = Integer.parseInt(term);
-		} 
-		catch(NumberFormatException e){
-	        tes = false;
-	    }
-		if (tes) {
-			List<MataKuliahKurikulumModel> matkul = matkulKurikulumService.selectMatkulTerm(term_matkul);
-			Map <String, List<MataKuliahKurikulumModel>> map = new HashMap<>();
-			map.put("matkul", matkul);
-				
-			if(matkul.size() > 0) {
 				api.setResult(map);
 				api.setMsg("success");
 				api.setStatus(200);
