36315a455dc139da079b95515b3346e07c99c8fd
Najwa <najwa.satirah@ui.ac.id>
2017-12-12 13:28:49 +0700

    dummy coba ngambil list dr api

--- ta_d1project/src/main/java/com/example/service/UniversitasService.java
+++ ta_d1project/src/main/java/com/example/service/UniversitasService.java
@@ -1,5 +1,7 @@
 package com.example.service;
 
+import java.util.List;
+
 import com.example.model.ApiModel;
 import com.example.model.FakultasModel;
 import com.example.model.ProdiModel;;
@@ -7,4 +9,5 @@ import com.example.model.ProdiModel;;
 public interface UniversitasService {
 	FakultasModel selectFakultas(int id_univ, int id_fakultas);
 	ProdiModel selectProdi (int id_univ, int id_fakultas, int id_prodi);
+	List<FakultasModel> selectAngkatan(int id_univ, int id_fakultas, int id_prodi);
 }
