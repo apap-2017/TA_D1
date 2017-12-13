c3bb18e6ad853396ee836d33f710205b5bf5e700
emmasharahwati <emmasharahh@gmail.com>
2017-12-13 16:14:12 +0700

    [EMMA] refactor

--- src/main/java/com/example/dao/UniversitasDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.dao;
-
-
-import com.example.model.ApiModel;
-
-public interface UniversitasDAO {
-	ApiModel selectFakultas(int id_univ, int id_fakultas);
-	ApiModel selectProdi (int id_univ, int id_fakultas, int id_prodi);
-}
